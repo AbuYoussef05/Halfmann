cf1c7fe045cecc8408fc0e8a9941816dd145ccd0
AbuYoussef05 <115083073+AbuYoussef05@users.noreply.github.com>
2025-11-08 19:42:53 +0100

    Delete client directory

--- client/src/components/examples/ContactTeaser.tsx
+++
@@ -1,9 +0,0 @@
-import ContactTeaser from '../ContactTeaser';
-
-export default function ContactTeaserExample() {
-  return (
-    <div className="p-8">
-      <ContactTeaser />
-    </div>
-  );
-}
